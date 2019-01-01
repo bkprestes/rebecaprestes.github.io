e78cb8e6ac3e3698889450cdfbaf6ca3ae6bdaf1
Rebeca <rebecaprestes@live.com>
2019-01-01 16:33:05 -0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./parts/Header";
+import Content from "./parts/Content";
+import Footer from "./parts/Footer";
 
 class App extends Component {
   render() {
+    const header = {
+      title: "Rebeca Prestes",
+      description: "Front-end Web Developer",
+      linkButton: "#portfolio",
+      nameButton: "View Portfolio"
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header {...header} />
+        <Content />
+        <Footer />
       </div>
     );
   }
